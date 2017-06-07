e58e92c6b7c2cc80d44d339306917f14aca369bb
rixhack <rafaelferrer@gmx.es>
2017-06-07 11:26:25 +0200

    Added comments in TwitterRequest class.

--- src/ldg/library/TwitterRequest.java
+++ src/ldg/library/TwitterRequest.java
@@ -4,13 +4,13 @@ import processing.core.PApplet;
 import controlP5.*;
 
 public class TwitterRequest {
-	PApplet parent;
-	int pValue = 0;
-	String query;
-	int maxValue = Integer.MAX_VALUE;
-	int minValue = 0;
-	int first = 0;
-	SecondApplet sa;
+	PApplet parent; // The processing script which runs the library.
+	int pValue = 0; // The value returned to the script.
+	String query; // The twitter query. (It will be used as a tag for the slider.)
+	int maxValue = Integer.MAX_VALUE; // The maximum value for the slider.
+	int minValue = 0; // The minimum value for the slider.
+	int first = 0; // 1 if getValue has already been called once, 0 otherwise.
+	SecondApplet sa; // The Applet which contains the slider.
 	
 	public TwitterRequest(PApplet parent, String query) {
 		this.parent = parent;
@@ -20,19 +20,22 @@ public class TwitterRequest {
 	
 	
 	public int getValue(){
+		// If it is the first time, create and run the applet with the slider.
 		if (first == 0){
-			String[] args = {"matrix_Simulator"};
+			String[] args = {"twitter_Simulator"};
 			sa = new SecondApplet();
 			PApplet.runSketch(args, sa);
 			first = 1;
 		}
-		return pValue;
+		return pValue; // Returns the last value from the slider.
 	}
 	
+	// Set maximum value for the slider.
 	public void setMaxValue(int v){
 		this.maxValue = v;
 	}
 	
+	// Set minimum value for the slider.
 	public void setMinValue(int v){
 		this.minValue = v;
 	}
@@ -47,23 +50,26 @@ public class TwitterRequest {
 		  Slider s;
 		  
 		  public void settings() {
-		    size(250, (int) 375);
+		    size(250, (int) 375); // Size of the slider window.
 		  }
 		  public void setup() {
 		    myController = new ControlP5(this);
 		    s = myController.addSlider(query,minValue,maxValue,minValue,100, 63, 50, 250);
+		    // Set markers and labels colour to black.
 		    s.setColorLabel(0);
 		    s.setColorTickMark(0);
 		    s.setColorValue(0);
 		    s.setColorValueLabel(0);
+		    // Set the slider to increase 1 by 1 with integer values.
 		    s.setNumberOfTickMarks(maxValue-minValue+1);
 		  }
 		  public void draw() {
-		    background(247);
+		    background(247); // Set the background to light gray.
 		    fill(0);
-		    pValue = (int) s.getValue();
+		    pValue = (int) s.getValue(); // Store in pValue the value from the slider each frame.
 		  }
 		  
+		  // Enable the slider to be modified with the keyboard.
 		  public void keyPressed(){
 			  if (keyCode == 38){
 			    s.setValue(s.getValue()+1);

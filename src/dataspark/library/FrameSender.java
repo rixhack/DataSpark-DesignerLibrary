340235f079d53ff4ba6f82cfb17b12b41670f713
rixhack <rafaelferrer@gmx.es>
2017-07-06 13:49:34 +0200

    Added stub frame sender.

---
+++ src/dataspark/library/FrameSender.java
@@ -0,0 +1,15 @@
+package dataspark.library;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class FrameSender {
+
+	public FrameSender(PApplet parent){
+		
+	}
+	
+	public void send(PImage img){
+		
+	}
+}
